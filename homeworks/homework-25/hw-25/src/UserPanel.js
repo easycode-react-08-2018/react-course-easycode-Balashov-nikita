722d19c7e86ebcb328f38694d8497c1fa0156b93
Balashov Nikita <reses142@gmail.com>
2018-09-02 22:32:34 +0300

    homework-25

---
+++ homeworks/homework-25/hw-25/src/UserPanel.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default function UserPanel(props) {
+    return (
+        <div className="panel panel-info">
+        <div className="panel-heading">
+          <h3 className="panel-title">{props.user.fullName}</h3>
+        </div>
+        <div className="panel-body">
+          <div className="row">
+            <div>
+              <div className="col-md-3 col-lg-3 " align="center">
+                <img src={props.user.avatarUrl} className="pull-left" alt="avatar"/>
+              </div>
+              <div className=" col-md-9 col-lg-9">
+                <table className="table table-user-information">
+                  <tbody>
+                    <tr>
+                      <td>Дата рождения</td>
+                      <td>{props.user.birthdate}</td>
+                    </tr>
+                    <tr>
+                      <td>Пол</td>
+                      <td>{props.user.gender}</td>
+                    </tr>
+                    <tr>
+                      <td>Адрес</td>
+                      <td>{props.user.address}</td>
+                    </tr>
+                    <tr>
+                      <td>Email</td>
+                      <td><a href="#">{props.user.email}</a></td>
+                    </tr>  
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
